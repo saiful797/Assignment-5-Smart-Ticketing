ae56927d3cc6c08f1810be82341822868ca80518
saiful797 <saifulislamsaif797@gmail.com>
2024-02-18 21:17:25 +0600

    Set seat, class and price in innerHtml

--- JS/bus-ticket.js
+++ JS/bus-ticket.js
@@ -18,7 +18,27 @@ for(let i=0; i<getButtonByClassName.length; i++){
     const seat = getButtonByClassName[i];
     // console.log(seat);
     seat.addEventListener('click', function(){
-        console.log('clicked');
+        // console.log('clicked');
+
+        //console.log(seat.querySelector("h5").innerText);
+
+        const seatItem = document.getElementById('seat-item');
+        const seatClass = document.getElementById('seat-class');
+        const seatPrice = document.getElementById('seat-price');
+
+        const h1 = document.createElement("h1");
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+
+        h1.innerText = seat.querySelector("h5").innerText;
+        h2.innerText ="Economy"
+        h3.innerText ="550"
+
+        seatItem.appendChild(h1);
+        seatClass.appendChild(h2);
+        seatPrice.appendChild(h3);
+
+        
     });
 }
 
