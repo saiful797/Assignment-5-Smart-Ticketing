eb4645890bb00ce7588ec6f8bbcfcaa6bb76f90a
saiful797 <saifulislamsaif797@gmail.com>
2024-02-18 20:30:10 +0600

    added btn property

--- JS/bus-ticket.js
+++ JS/bus-ticket.js
@@ -8,3 +8,17 @@ buyTickets.addEventListener('click', function(){
     busSeats.scrollIntoView({ behavior: 'smooth' });
     
 });
+
+//Select seats and Buy seats
+
+const getButtonByClassName = document.querySelectorAll(".btn-seat");
+// console.log(getButtonByClassName);
+
+for(let i=0; i<getButtonByClassName.length; i++){
+    const seat = getButtonByClassName[i];
+    // console.log(seat);
+    seat.addEventListener('click', function(){
+        console.log('clicked');
+    });
+}
+
